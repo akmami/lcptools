a1f7deae2f7e0bbe89726c94d101ffe145dc27dd
Akmuhammet Ashyralyyev <akmuhammet99@gmail.com>
2023-12-08 19:21:24 +0300

    gap fixes in fastq

--- experiment/cpp-lcp-fastq-error.cpp
+++ experiment/cpp-lcp-fastq-error.cpp
@@ -10,31 +10,42 @@
 
 
 inline void process(std::string &str ) {
+    
+    int index = str.find_last_of(' ');
     std::string processed = "";
-    for ( std::string::iterator it = str.begin(); it != str.end(); it++ ) {
+    
+    for ( std::string::iterator it = str.begin() + index + 1; it != str.end(); it++ ) {
         if ( (*it) != '-') {
             processed += (*it);
+        }
+        if ( (*it) == 'N' ) {
+            str = "";
+            return;
         }
     }
     str = processed;
 }
 
 
-inline void analyze( lcp::string *str1, lcp::string *str2, int &deletion, int &insertion, int &match, int &mismatch ) {
+inline void analyze( lcp::string* str1, lcp::string* str2, int &deletion, int &insertion, int &match, int &mismatch ) {
 
     int m = str1->cores.size(), n = str2->cores.size(), i, j, index;
     int arr1[m], arr2[n];
     
-    for ( std::deque<lcp::core*>::iterator it = str1->cores.begin(); it != str1->cores.end(); it++ ) {
+    index = 0;
+    for ( std::deque<lcp::core*>::iterator it = str1->cores.begin(); it != str1->cores.end(); it++, index++ ) {
         arr1[index] = (*it)->label();
     }
     
     index = 0;
-    for ( std::deque<lcp::core*>::iterator it = str2->cores.begin(); it != str2->cores.end(); it++ ) {
+    for ( std::deque<lcp::core*>::iterator it = str2->cores.begin(); it != str2->cores.end(); it++, index++ ) {
         arr2[index] = (*it)->label();
     }
-
-    int dp[m+1][n+1] = {0};
+    
+    int **dp = new int*[m+1];
+    for ( int i = 0; i < m+1; i++ ) {
+        dp[i] = new int[n+1];
+    }
  
     for (i = 0; i <= m; i++) { dp[i][0] = i; }
     for (i = 0; i <= n; i++) { dp[0][i] = i; }    
@@ -51,7 +62,6 @@ inline void analyze( lcp::string *str1, lcp::string *str2, int &deletion, int &i
     // Finding deletion, insertion and mismatch count
     i = m; j = n;
 
-    std::cout << "Finding deletion, insertion and mismatch count" << std::endl;
     while ( !(i == 0 || j == 0)) {
         if (arr1[i - 1] == arr2[j - 1]) { i--; j--; match++; }
         else if (dp[i - 1][j - 1] + 1 == dp[i][j]) { i--; j--; mismatch++; }
@@ -59,6 +69,11 @@ inline void analyze( lcp::string *str1, lcp::string *str2, int &deletion, int &i
         else if (dp[i][j - 1] + 1 == dp[i][j]) { j--; insertion++; }
         //else { std::cout << "else" << std::endl; }
     }
+    
+    for ( int i = 0; i < m+1; i++ ) {
+        delete dp[i];
+    }
+    delete [] dp;
 };
 
 
@@ -75,21 +90,15 @@ int main(int argc, char **argv) {
         return -1;
     }
 
-    std::ifstream input2(argv[1]);
-    if (!input2.good()) {
-        std::cerr << "Error opening: " << argv[2] << " . You have failed." << std::endl;
-        return -1;
-    }
 
     // initializing coefficients of the alphabet
     lcp::init_coefficients();
 
     // variables
-    std::string line_original, line_error;
+    std::string line, str1, str2;
 
-    std::fstream genome_original, genome_error;
-    genome_original.open(argv[1], std::ios::in);
-    genome_error.open(argv[2], std::ios::in);
+    std::fstream maf_file;
+    maf_file.open(argv[1], std::ios::in);
 
     int read_count = 0;
     int deletion = 0;
@@ -98,57 +107,41 @@ int main(int argc, char **argv) {
     int mismatch = 0;
     
 	// read file
-    if ( genome_original.is_open() && genome_error.is_open() ) {  
+    if ( maf_file.is_open() ) {  
 
         std::cout << "Program begins" << std::endl;
 
-        while ( true ) {
-            std::cout << "processing" << std::endl;
-            if ( getline(genome_original, line_original) && getline(genome_error, line_error) ) {
-
-                read_count++;
-
-                // line is id
-
-                // raw sequence
-                getline(genome_original, line_original);
-                getline(genome_error, line_error);
-
-                std::cout << "trimming started" << std::endl;
-                process( line_original );
-                std::cout << "trimmin ended" << std::endl;
-                
-                lcp::string *str_original = new lcp::string(line_original);
-                lcp::string *str_error = new lcp::string(line_error);
-
-                for ( int i = 1; i < LCP_LEVEL; i++ ) {
-                    str_original->deepen();
-                    str_error->deepen();
-                }
-
-                analyze( str_original, str_error, deletion, insertion, match, mismatch );
-                // std::cout << "processing started" << std::endl;
-                // analyze( line_original, line_error, deletion, insertion, mismatch );
-                // std::cout << "processing done" << std::endl;
-
-                delete str_original;
-                delete str_error;
-
-                // +
-                getline(genome_original, line_original);
-                getline(genome_error, line_error);
-                
-                // quality
-                getline(genome_original, line_original);
-                getline(genome_error, line_error);
+        while ( getline(maf_file, line) ) {
             
-            } else {
-                break;
+            read_count++;
+
+            // line is 'a'
+
+            // raw sequence
+            getline(maf_file, str1);
+            getline(maf_file, str2);
+
+            process( str1 );
+            process( str2 );
+
+            lcp::string *str_original = new lcp::string(str1);
+            lcp::string *str_error = new lcp::string(str2);
+
+            for ( int i = 1; i < LCP_LEVEL; i++ ) {
+                str_original->deepen();
+                str_error->deepen();
             }
+
+            analyze( str_original, str_error, deletion, insertion, match, mismatch );
+
+            delete str_original;
+            delete str_error;
+
+            // empty line
+            getline(maf_file, line);
         }
                 
-        genome_original.close();
-        genome_error.close();
+        maf_file.close();
     }
 
     std::cout << "Deletion:     " << deletion << std::endl;
