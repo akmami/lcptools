2557d6ad90fe138be9961df963b09f125950fd09
Akmuhammet Ashyralyyev <akmuhammet99@gmail.com>
2024-01-14 15:18:45 +0300

    maf processor implemented. (remove '-' form reads)

--- experiment/process-maf.cpp
+++ experiment/process-maf.cpp
@@ -1,188 +1,101 @@
 #include <fstream>
 #include <iostream>
 #include <sstream>
-#include "../string.cpp"
+#include "GzFile.hpp"
 
-#define MAX_ROWS            100000
-#define MAX_COLUMNS         100000
-#define GAP_PENALTY         -2
-#define MATCH               1
-#define MISMATCH            -1
 
-bool process( std::string &str ) {
+#define BUFFERSIZE  100000
+
+/**
+ * A function to remove '-' from the given string
+ */
+void process( char str[BUFFERSIZE], std::string &line ) {
     
-    size_t writeIndex = 0;
+    size_t writeIndex = 0, readIndex = 0;
 
-    for (size_t readIndex = 0; readIndex < str.size(); readIndex++) {
+    for ( ; readIndex < BUFFERSIZE && str[readIndex] != '\n'; readIndex++) {
         if (str[readIndex] != '-') {
             str[writeIndex++] = str[readIndex];
         }
     }
 
-    str.erase( str.end() - (str.size() - writeIndex ), str.end() );
+    str[writeIndex] = '\0';
 
-    return true;
-};
-
-bool align(lcp::string* seq1, lcp::string* seq2, int** dp, int& deletion, int& insertion, int& match, int& mismatch ) {
-    int rows = seq1->cores.size() + 1;
-    int cols = seq2->cores.size() + 1;
-   
-    for (int i = 1; i < rows; ++i) {
-        dp[i][0] = dp[i - 1][0] + 1;
-    }
-
-    for (int j = 1; j < cols; ++j) {
-        dp[0][j] = dp[0][j - 1] + 1;
-    }
-
-    for (int i = 1; i < rows; ++i) {
-        for (int j = 1; j < cols; ++j) {
-
-            dp[i][j] = std::max(
-                std::max(
-                    dp[i - 1][j] + GAP_PENALTY ,
-                    dp[i][j - 1] + GAP_PENALTY
-                ), 
-                    dp[i - 1][j - 1] + ( seq1->cores.at(i - 1) == seq2->cores.at(j - 1) ? MATCH : MISMATCH )
-                );
-        }
-    }
-
-    // Traceback to find the alignment
-    int i = rows - 1;
-    int j = cols - 1;
-
-    while (i > 0 || j > 0) {
-
-        if (i > 0 && j > 0 && dp[i][j] == dp[i - 1][j - 1] + ( seq1->cores.at(i - 1) == seq2->cores.at(j - 1) ? MATCH : MISMATCH ) ) {
-            // Match or mismatch
-            if ( seq1->cores.at(i - 1) == seq2->cores.at(j - 1) ) {
-                ++match;
-            } else {
-                ++mismatch;
-            }
-            --i;
-            --j;
-        } else if (i > 0 && dp[i][j] == dp[i - 1][j] + GAP_PENALTY) {
-            ++deletion;
-            --i;
-        } else {
-            ++insertion;
-            --j;
-        }
-    }
-
-    return true;
+    line.clear();
+    line.append(str);
 };
 
 int main(int argc, char **argv) {
 
-    if (argc < 3) {
-        std::cerr << "Wrong format: " << argv[0] << " [infile] [lcp-level]" << std::endl;
+    if (argc != 3) {
+        std::cerr << "Wrong format: " << argv[0] << " [infile] [outfile]" << std::endl;
         return -1;  
     }
 
-    std::ifstream input1(argv[1]);
-    if (!input1.good()) {
-        std::cerr << "Error opening: " << argv[1] << " . You have failed." << std::endl;
-        return -1;
+    // validate input file
+    GzFile maffile( argv[1], "rb");
+    if (!maffile) {
+        std::cerr << "Failed to open file: " << argv[1] << std::endl;
+        return 1;
     }
 
-    int lcp_level = atoi(argv[2]);
-
-    int** dp = new int*[MAX_ROWS];
-    for (int i = 0; i < MAX_ROWS; ++i) {
-        dp[i] = new int[MAX_COLUMNS];
+    // open output file
+    std::ofstream outfile(argv[2]);
+    if (!outfile) {
+        std::cerr << "Error creating outfile " << argv[1] << std::endl;
+        return 1;
     }
-
-    int deletion = 0, insertion = 0, match = 0, mismatch = 0, read_count = 0;
-
-    // initializing coefficients of the alphabet
-    lcp::init_coefficients();
 
     // variables
     std::string line, seq1, seq2;
-
-    std::fstream maf_file;
-    maf_file.open(argv[1], std::ios::in);
-
-    std::ofstream output("output.txt");
-
-    if (!output.is_open()) {
-        std::cerr << "Error opening file: " << argv[2] << std::endl;
-        return -1;
-    }
+    char buffer[BUFFERSIZE];
 
 	// read file
-    if ( maf_file.is_open() ) {  
+    while ( true ) {
 
-        std::cout << "Program begins" << std::endl;
-
-        while ( std::getline(maf_file, line) ) {
-            
-            read_count++;
-
-            // line is 'a'
-
-            // Raw Sequence 1
-            std::getline(maf_file, line);
-
-            std::istringstream iss(line);
-
-            // Ignore the first six columns
-            for (int j = 0; j < 6; ++j) {
-                iss >> seq1; 
+        if ( maffile.gets(buffer, sizeof(buffer)) == Z_NULL) {
+            // End of file or an error
+            if ( ! maffile.eof() ) {
+                std::cerr << "Error reading file." << std::endl;
             }
-            // Access the 7th column
-            iss >> seq1;
-
-            // Raw Sequence 2
-            std::getline(maf_file, line);
-            
-            iss.clear();
-            iss.str(line);
-
-            // Ignore the first six columns
-            for (int j = 0; j < 6; ++j) {
-                iss >> seq2; 
-            }
-
-            // Access the 7th column
-            iss >> seq2;
-
-            // empty line
-            getline(maf_file, line);
-
-            process(seq1);
-            process(seq2);
-
-            lcp::string *str1 = new lcp::string(seq1);
-            lcp::string *str2 = new lcp::string(seq2);
-
-            for (int i = 1; i < lcp_level; i++) {
-                str1->deepen();
-                str2->deepen();
-            }
-
-            align(str1, str2, dp, deletion, insertion, match, mismatch);
-
-            std::cout << str1 << std::endl;
-            std::cout << str2 << std::endl;
+            break;
         }
-                
-        maf_file.close();
-    }
+        // line is 'a'
 
-    std::cout << "Deletion:     " << deletion << std::endl;
-    std::cout << "Insertion:    " << insertion << std::endl;
-    std::cout << "Match:    " << match << std::endl;
-    std::cout << "Mismatch:     " << mismatch << std::endl;
+        // get raw sequence 1
+        maffile.gets(buffer, sizeof(buffer));
+        
+        // remove '-' from raw sequence 1
+        process(buffer, line);
 
-    for (int i = 0; i < MAX_ROWS; ++i) {
-        delete dp[i];
+        // parse string
+        std::istringstream iss(line);
+
+        // access the 7th column
+        iss >> seq1 >> seq1 >> seq1 >> seq1 >> seq1 >> seq1 >> seq1; 
+
+        outfile << seq1 << std::endl;
+
+        line.clear();
+
+        // get raw sequence 2
+        maffile.gets(buffer, sizeof(buffer));
+
+        // remove '-' from raw sequence 2
+        process(buffer, line);
+        
+        // parse string
+        iss.clear();
+        iss.str(line);
+
+        // access the 7th column
+        iss >> seq2 >> seq2 >> seq2 >> seq2 >> seq2 >> seq2 >> seq2;
+
+        outfile << seq2 << std::endl;
+
+        // empty line
+        maffile.gets(buffer, BUFFERSIZE);
     }
-    delete dp;
 
     return 0;
 };
