d5163e0dd9f384820b32e25607af33e220b85678
Akmuhammet Ashyralyyev <akmuhammet99@gmail.com>
2024-01-16 14:18:17 +0300

    print to gz file func impl

--- experiment/GzFile.hpp
+++ experiment/GzFile.hpp
@@ -26,6 +26,19 @@ public:
         return gzFile_ ? gzgets(gzFile_, buffer, len) : Z_NULL;
     }
 
+    // Print to compressed file
+    int printf(const char* format, ...) {
+        char buffer[BUFFERSIZE];
+        va_list args;
+        va_start(args, format);
+        int ret = vsnprintf(buffer, BUFFERSIZE, format, args);
+        va_end(args);
+        if (ret > 0 && ret < BUFFERSIZE) {
+            gzwrite(gzFile_, buffer, ret);
+        }
+        return ret;
+    }
+
     // Check if the file is valid
     explicit operator bool() const {
         return gzFile_ != nullptr;
