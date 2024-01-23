7d4be364b7ae7db7973082e4be1c412acebb1411
Akmuhammet Ashyralyyev <akmuhammet99@gmail.com>
2024-01-23 18:37:22 +0300

    total links added, reverse link considered

--- experiment/compare-gfa.cpp
+++ experiment/compare-gfa.cpp
@@ -4,7 +4,9 @@
 #include <unordered_set>
 #include <sstream>
 
-std::string parse_link(const std::string& line, std::unordered_set<std::string>& links, bool considerRev) {
+const bool CONSIDER_REV = true;
+
+void parse_link(const std::string& line, std::unordered_set<std::string>& links, bool considerRev) {
     std::istringstream iss(line);
     std::string segment, read1, read2;
 
@@ -19,11 +21,9 @@ std::string parse_link(const std::string& line, std::unordered_set<std::string>&
             links.insert(read2 + "-" + read1);
         }
     }
-
-    return "";
 }
 
-std::unordered_set<std::string> read_links(const std::string& filename) {
+std::unordered_set<std::string> read_links(const std::string& filename, bool considerRev) {
     std::unordered_set<std::string> links;
     std::ifstream infile(filename);
     std::string line;
@@ -35,10 +35,7 @@ std::unordered_set<std::string> read_links(const std::string& filename) {
 
     while (getline(infile, line)) {
         if (line[0] == 'L') {
-            std::string link = parse_link(line);
-            if (!link.empty()) {
-                insert(link, links, true);
-            }
+            parse_link(line, links, considerRev);
         }
     }
 
@@ -55,8 +52,8 @@ int main(int argc, char** argv) {
     std::string gold(argv[1]);
     std::string other(argv[2]);
 
-    std::unordered_set<std::string> gold_links = read_links(gold);
-    std::unordered_set<std::string> other_links = read_links(other);
+    std::unordered_set<std::string> gold_links = read_links(gold, CONSIDER_REV);
+    std::unordered_set<std::string> other_links = read_links(other, CONSIDER_REV);
 
     int correct = 0;
     int incorrect = 0;
@@ -69,8 +66,15 @@ int main(int argc, char** argv) {
         }
     }
 
-    std::cout << "Correct links: " << correct << std::endl;
-    std::cout << "Incorrect links: " << incorrect << std::endl;
+    std::cout << "Correct links:    " << correct << std::endl;
+    std::cout << "Incorrect links:  " << incorrect << std::endl;
+
+    if ( CONSIDER_REV ) {
+        std::cout << "Total:            " << gold_links.size() / 2 << std::endl;
+    } else {
+        std::cout << "Total:            " << gold_links.size() << std::endl;
+    }
+    
 
     return 0;
 }
