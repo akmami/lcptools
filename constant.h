ed3d21305200d73e3aa697ae931b319fda033946
Akmuhammet Ashyralyyev <akmuhammet99@gmail.com>
2024-07-02 15:41:17 +0300

    constants moved to seperate file

---
+++ constant.h
@@ -0,0 +1,11 @@
+#ifndef   CONSTANT_H
+#define   CONSTANT_H
+
+typedef unsigned char uchar;
+
+#define SIZE_PER_BLOCK                  32
+#define COMPRESSION_ITERATION_COUNT     1
+#define MAXIMUM_LENGTH                  10000
+#define CONSTANT_FACTOR                 2
+
+#endif
