36fe2af60350466b3244a9563f6fa10901edf910
Akmuhammet Ashyralyyev <akmuhammet99@gmail.com>
2024-07-03 18:02:23 +0300

    base_core write to file binary impl

--- base_core.h
+++ base_core.h
@@ -2,7 +2,7 @@
 #define   BASE_CORE_H
 
 #include <vector>
-#include <ostream>
+#include <fstream>
 #include <iterator>
 #include "constant.h"
 #include "encoding.h"
@@ -10,23 +10,25 @@
 namespace lcp {
 	class base_core {
     public:
-        // Represenation related variables
-		uchar* p;
-		int block_number;
-		int start_index;
-
-		// Core related variables
+        // Core related variables
 		int start;
 		int end;
+
+		// Represenation related variables
+		int block_number;
+		int start_index;
+		uchar* p;
 
 		base_core(std::string::iterator it1, std::string::iterator it2, int start, bool rev_comp = false);
 		base_core(std::vector<base_core*>::iterator it1, std::vector<base_core*>::iterator it2);
 		base_core(uchar* p, int block_number, int start_index, int start, int end);
+		base_core(std::ifstream& in);
 		~base_core();
 
         bool get(int index) const;
 		uint compress(const base_core* other);
 		uint label();
+		void write(std::ofstream& out);
 	};
 
     // base_core operator overloads
