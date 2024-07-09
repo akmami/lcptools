e50be0ba8a16abd4f5f982f950857e7332213ca1
Akmuhammet Ashyralyyev <akmuhammet99@gmail.com>
2024-07-09 15:38:09 +0300

    name change: string -> lps (locally parsed string)

--- lps.h
+++ lps.h
@@ -1,10 +1,11 @@
-#ifndef STRING_H
-#define STRING_H
+#ifndef LPS_H
+#define LPS_H
 
 #include <string>
 #include <vector>
 #include <fstream>
 #include <iterator>
+#include <algorithm>
 #include "constant.h"
 #include "encoding.h"
 #include "base_core.h"
@@ -12,7 +13,7 @@
 
 namespace lcp {
 
-    class string {
+    class lps {
     public:
         int level;
         size_t start_index;     // For each DCT, first core is not compressed, hence needs to be removed.
@@ -23,19 +24,19 @@ namespace lcp {
         std::vector<base_core*> base_cores;
         
 
-        string(std::string &str, bool rev_comp = false);
-        string(std::ifstream& in);
-        ~string();
+        lps(std::string &str, bool rev_comp = false);
+        lps(std::ifstream& in);
+        ~lps();
 
-        void process_string(std::string::iterator it1, std::string::iterator it2, std::string::iterator end, bool rev_comp = false);
+        void parse(std::string::iterator it1, std::string::iterator it2, std::string::iterator end, bool rev_comp = false);
         bool deepen();
         bool deepen(int lcp_level);
         void write(std::string filename);
         void write(std::ofstream& out);
     };
 
-    std::ostream& operator<<(std::ostream& os, const string& element);
-    std::ostream& operator<<(std::ostream& os, const string* element);
+    std::ostream& operator<<(std::ostream& os, const lps& element);
+    std::ostream& operator<<(std::ostream& os, const lps* element);
 };
 
 #endif
