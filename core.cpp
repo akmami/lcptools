bf41a5aecc1994d8f5bffc151a68de37543cb409
Akmuhammet Ashyralyyev <akmuhammet99@gmail.com>
2024-09-06 15:02:43 +0300

    base_core removed, only array based representation of core type left

--- core.cpp
+++ core.cpp
@@ -1,74 +1,106 @@
 #include "core.h"
 
 namespace lcp {
-
-	void core::compress(const core* other) {
-
-		uint t_label = this->label, o_label = other->label;
-		uchar bit_count = this->label_length > other->label_length ? other->label_length : this->label_length;
-		uint new_label = 0;
-		
-		while (bit_count-- > 0 && t_label % 2 == o_label % 2) {
-			t_label /= 2;
-			o_label /= 2;
-			new_label++;
-		}
-		
-		// shift left by 1 bit and set last bit to difference
-		new_label = 2 * new_label + t_label % 2;
-		
-		// Compressed value is: new_label
-
-		// Change this object according to  the new values represents compressed version.
-		this->label = new_label;
-		this->label_length = 0;
-
-		while (new_label) {
-			this->label_length++;
-			new_label /= 2;
-		}
-		
-		this->label_length = this->label_length > 2 ? this->label_length : 2;
-	};
 	
-	core::core(std::vector<lcp::base_core*>::iterator it1, std::vector<lcp::base_core*>::iterator it2) {
-		
-		this->start = (*it1)->start;
-		this->end = (*(it2-1))->end;
+	core::core(std::string::iterator begin, std::string::iterator end, size_t start_index, size_t end_index, bool rev_comp) {
 
-		this->label = 0;
-		this->label_length = 0;
+		int* coefficientsArray = ( rev_comp ? reverse_complement_coefficients : coefficients);
 
-		// Concatinating cores
-		uchar index = 0;
-		
-		for( std::vector<base_core*>::iterator it = it2-1; it != it1-1; it-- ) {
-			this->label |= (*it)->label() << index;
-			index += (*it)->block_number * SIZE_PER_BLOCK - (*it)->start_index;
+		this->start = start_index;
+		this->end = end_index;
+
+		this->block_number = ( (end_index - start_index) * dict_bit_size - 1) / SIZE_PER_BLOCK + 1 ;
+		this->start_index = this->block_number * SIZE_PER_BLOCK - (end_index - start_index) * dict_bit_size;
+
+		// make allocation for the bit representation
+		// std::cout << "block_number: " << this->block_number << std::endl;
+		// std::cout << "start_index: " << start_index << std::endl;
+		// std::cout << "end_index: " << end_index << std::endl;
+		this->p = new ublock[this->block_number];
+
+		// clear dumps
+		for (size_t i = 0; i < this->block_number; i++) {
+			this->p[i] = 0;
 		}
 
-		this->label_length = index > 31 ? 32 : index;
-	};
+		// encoding string to bits
+		int coefficient, shift;
+		size_t index = this->start_index, block_index;
 
-	core::core(std::vector<core*>::iterator it1, std::vector<core*>::iterator it2) {
-		
-		this->start = (*it1)->start;
-		this->end = (*(it2-1))->end;
-		
-		this->label = 0;
-		this->label_length = 0;
-		
-		// Concatinating cores		    
-		for( std::vector<core*>::iterator it = it2-1; it != it1-1; it-- ) {
-			this->label |= (*it)->label << this->label_length;
-			this->label_length += (*it)->label_length;
+		for (std::string::iterator it = begin; it != end; it++) {
+			coefficient = coefficientsArray[static_cast<unsigned char>(*it)];
+			block_index = index / SIZE_PER_BLOCK;
+			shift = SIZE_PER_BLOCK - ( index % SIZE_PER_BLOCK ) - dict_bit_size;
+
+			if (shift >= 0) {
+				this->p[block_index] |= (coefficient << shift);
+			} else {
+				this->p[block_index] |= (coefficient >> (-shift));
+				if (block_index + 1 < this->block_number) {
+					this->p[block_index + 1] |= (coefficient << (SIZE_PER_BLOCK + shift));
+				}
+			}
+			index += dict_bit_size;
 		}
-		this->label_length = this->label_length > 31 ? 32 : this->label_length;
+
+		coefficientsArray = nullptr;
 	};
 
-	core::core(uint label, uchar label_length, int start, int end) {
-		this->label = label;
-		this->label_length = label_length;
+	core::core(std::vector<core*>::iterator begin, std::vector<core*>::iterator end) {
+		this->start = (*begin)->start;
+		this->end = (*(end-1))->end;
+
+		// calculate required number of bits to represent core
+		size_t bit_size = 0;
+		for ( std::vector<core*>::iterator it = begin; it != end; it++ ) {
+			bit_size += (*it)->block_number * SIZE_PER_BLOCK - (*it)->start_index;
+		}
+		
+		this->block_number = (bit_size - 1) / SIZE_PER_BLOCK + 1;
+		this->start_index = this->block_number * SIZE_PER_BLOCK - bit_size;
+		
+		// make allocation for the bit representation
+		this->p = new ublock[this->block_number];
+
+		// clear dumps
+		for( size_t i = 0; i < this->block_number; i++ ) {
+			this->p[i] = 0;
+		}
+
+		size_t index = this->start_index, block_index, block_size;
+		int block, shift;
+
+		for( std::vector<core*>::iterator it = begin; it < end; it++ ) {
+
+			for ( size_t i = 0; i < (*it)->block_number; i++ ) {
+				block_index = index / SIZE_PER_BLOCK;
+				block = (*it)->p[i];
+				block_size = ( i == 0 ? SIZE_PER_BLOCK - (*it)->start_index : SIZE_PER_BLOCK );
+				shift = SIZE_PER_BLOCK - ( index % SIZE_PER_BLOCK ) - block_size;
+
+				// shift and paste
+				
+				// if after shifting, there is overflow
+				// always overflow occurs if block index is not 0 or if so, 
+				// number of bits in first block + index is causing overflow.
+				// std::cout << "index: " << index << ", block: " << block << ", block_size: " << block_size << ", shift: " << shift << std::endl;
+				if (shift >= 0) {
+					this->p[block_index] |= (block << shift);
+				} else {
+					this->p[block_index] |= (block >> (-shift));
+					if (block_index + 1 < this->block_number) {
+						this->p[block_index + 1] |= (block << (SIZE_PER_BLOCK + shift));
+					}
+				}
+				index += block_size;
+			}
+		}
+	};
+
+	core::core(ublock* p, size_t block_number, size_t start_index, size_t start, size_t end) {
+		this->p = p;
+		this->block_number = block_number;
+		this->start_index = start_index;
 		this->start = start;
 		this->end = end;
 	};
@@ -76,55 +108,242 @@ namespace lcp {
 	core::core(std::ifstream& in) {
 		in.read(reinterpret_cast<char*>(&start), sizeof(start));
         in.read(reinterpret_cast<char*>(&end), sizeof(end));
-        in.read(reinterpret_cast<char*>(&label), sizeof(label));
-        in.read(reinterpret_cast<char*>(&label_length), sizeof(label_length));
-    };
-	
-	void core::write(std::ofstream& out) {
+        in.read(reinterpret_cast<char*>(&block_number), sizeof(block_number));
+        in.read(reinterpret_cast<char*>(&start_index), sizeof(start_index));
+		this->p = new ublock[this->block_number];
+
+		for( size_t i = 0; i < this->block_number; i++ ) {
+			this->p[i] = 0;
+		}
+
+        in.read(reinterpret_cast<char*>(p), this->block_number * sizeof(unsigned char));
+    }
+
+	core::~core() {
+		delete[] p;
+		this->p = nullptr;
+	};
+
+	// delete in near future
+	uint core::label() {
+		uint label = 0;
+		for (int i = this->block_number - 1, index = 0; i >= 0 && index < 32; i--, index += SIZE_PER_BLOCK) {
+			if (index + SIZE_PER_BLOCK > 31) {
+				label |= (this->p[i] << index) & ((1u << (32 - index)) - 1);
+				break;
+			}
+			label |= (this->p[i] << index);
+		}
+		return label;
+	};
+
+	bool core::get(int index) const {
+		return ( this->p[(this->start_index + index) / SIZE_PER_BLOCK] & ( 1 << ( SIZE_PER_BLOCK - ( (this->start_index + index) % SIZE_PER_BLOCK ) - 1 ) ) );
+	};
+
+	void core::compress(const core* other) {
+		
+		size_t o_block_index = other->block_number - 1, t_block_index = this->block_number - 1;
+		ublock o = other->p[o_block_index], t = this->p[t_block_index];
+		size_t current_index = 0, new_bit_size = 0, temp = 0;
+		
+		while (o_block_index > 0 && t_block_index > 0 && o == t) {
+			o = other->p[--o_block_index];
+			t = this->p[--t_block_index];
+		}
+
+		current_index = o_block_index ? 
+			(t_block_index ? 
+				0 : 
+				this->start_index) : 
+			(t_block_index ? 
+				other->start_index :
+				std::max(other->start_index, this->start_index) );
+		
+
+		while (current_index < SIZE_PER_BLOCK && o % 2 == t % 2) {
+			o /= 2;
+			t /= 2;
+			current_index++;
+			temp++;
+		}
+		
+		// shift left by 1 bit and set last bit to difference
+		size_t new_label = 2 * ( (this->block_number - t_block_index - 1) * SIZE_PER_BLOCK + temp) + t % 2;
+
+		// count bits requred to reperesent new_label
+		new_bit_size = 0;
+		temp = new_label;
+		while(temp != 0) {
+			new_bit_size++;
+			temp /= 2;
+		}
+
+		new_bit_size = new_bit_size > 2 ? new_bit_size : 2;
+
+		// compressed value is: new_label
+		this->block_number = (new_bit_size - 1) / SIZE_PER_BLOCK + 1;
+		this->start_index = this->block_number * SIZE_PER_BLOCK - new_bit_size;
+		delete[] this->p;
+		this->p = nullptr;
+
+		// make allocation for the bit representation
+		this->p = new ublock[this->block_number];
+
+		// clear old dumps
+		for(size_t i=0; i<this->block_number; i++) {
+			this->p[i] = 0;
+		}
+
+		// Set bits block by block and avoid unnecesary assignments
+		int current_block = this->block_number - 1;
+
+		while ( new_label > 0 ) {
+			this->p[current_block] = (ublock)new_label;
+			new_label = new_label >> SIZE_PER_BLOCK;
+			current_block--;
+		}
+	};
+
+    void core::write(std::ofstream& out) {
 		out.write(reinterpret_cast<const char*>(&start), sizeof(start));
         out.write(reinterpret_cast<const char*>(&end), sizeof(end));
-        out.write(reinterpret_cast<const char*>(&label), sizeof(label));
-        out.write(reinterpret_cast<const char*>(&label_length), sizeof(label_length));
-    };
+        out.write(reinterpret_cast<const char*>(&block_number), sizeof(block_number));
+        out.write(reinterpret_cast<const char*>(&start_index), sizeof(start_index));
+        out.write(reinterpret_cast<char*>(p), block_number * sizeof(unsigned char));
+    }
 
 	// core operator overloads
 	bool operator == (const core& lhs, const core& rhs) {
-	    return lhs.label == rhs.label;
+	    size_t lhs_block_index = 0;
+		size_t rhs_block_index = 0;
+		
+		if (lhs.block_number < rhs.block_number) {
+			while(rhs.block_number - rhs_block_index != lhs.block_number) {
+				if (rhs.p[rhs_block_index] > 0) {
+					return false;
+				}
+				rhs_block_index++;
+			}
+		}
+		else if (lhs.block_number > rhs.block_number) {
+			while(lhs.block_number - lhs_block_index != rhs.block_number) {
+				if (lhs.p[lhs_block_index] > 0) {
+					return true;
+				}
+				lhs_block_index++;
+			}
+		}
+
+	    while( lhs_block_index < lhs.block_number ) {
+	    	if ( lhs.p[lhs_block_index] != rhs.p[rhs_block_index] ) {
+	    		return false;
+	    	}
+	    	lhs_block_index++;
+	    	rhs_block_index++;
+	    }
+	    return true;
 	};
 
 	bool operator > (const core& lhs, const core& rhs) {
-	    return lhs.label > rhs.label;
+		size_t lhs_block_index = 0;
+		size_t rhs_block_index = 0;
+		
+		if (lhs.block_number < rhs.block_number) {
+			while(rhs.block_number - rhs_block_index != lhs.block_number) {
+				if (rhs.p[rhs_block_index] > 0) {
+					return false;
+				}
+				rhs_block_index++;
+			}
+		}
+		else if (lhs.block_number > rhs.block_number) {
+			while(lhs.block_number - lhs_block_index != rhs.block_number) {
+				if (lhs.p[lhs_block_index] > 0) {
+					return true;
+				}
+				lhs_block_index++;
+			}
+		}
+
+	    while( lhs_block_index < lhs.block_number ) {
+	    	if ( lhs.p[lhs_block_index] > rhs.p[rhs_block_index] ) {
+	    		return true;
+	    	}
+	    	if ( lhs.p[lhs_block_index] < rhs.p[rhs_block_index] ) {
+	    		return false;
+	    	}
+	    	lhs_block_index++;
+	    	rhs_block_index++;
+	    }
+	    return false;
 	};
 
 	bool operator < (const core& lhs, const core& rhs) {
-	    return lhs.label < rhs.label;
+	    size_t lhs_block_index = 0;
+		size_t rhs_block_index = 0;
+		
+		if (lhs.block_number < rhs.block_number) {
+			while(rhs.block_number - rhs_block_index != lhs.block_number) {
+				if (rhs.p[rhs_block_index] > 0) {
+					return true;
+				}
+				rhs_block_index++;
+			}
+		}
+		else if (lhs.block_number > rhs.block_number) {
+			while(lhs.block_number - lhs_block_index != rhs.block_number) {
+				if (lhs.p[lhs_block_index] > 0) {
+					return false;
+				}
+				lhs_block_index++;
+			}
+		}
+
+	    while( lhs_block_index < lhs.block_number ) {
+	    	if ( lhs.p[lhs_block_index] < rhs.p[rhs_block_index] ) {
+	    		return true;
+	    	}
+	    	if ( lhs.p[lhs_block_index] > rhs.p[rhs_block_index] ) {
+	    		return false;
+	    	}
+	    	lhs_block_index++;
+	    	rhs_block_index++;
+	    }
+	    return false;
 	};
 
 	bool operator != (const core& lhs, const core& rhs) {
-	    return lhs.label != rhs.label;
+	    return !(lhs == rhs);
 	};
 
 	bool operator >= (const core& lhs, const core& rhs) {
-		return lhs.label >= rhs.label;
+		if (lhs < rhs)
+			return false;
+	    return true;
 	};
 
 	bool operator <= (const core& lhs, const core& rhs) {
-	    return lhs.label <= rhs.label;
+	    if (lhs > rhs)
+	    	return false;
+	    return true;
 	};
 
 	std::ostream& operator<<(std::ostream& os, const core& element) {
-		uchar bit_size = element.label_length;
-		while (bit_size--) {
-			os << ( (element.label >> bit_size) & 1 );
+		for (int block_index = element.block_number - 1; 0 <= block_index; block_index-- ){
+			for (size_t index = element.start_index; index < element.block_number * SIZE_PER_BLOCK; index++ ) {
+				os << ((element.p[index / SIZE_PER_BLOCK] >> (SIZE_PER_BLOCK - index % SIZE_PER_BLOCK)) & 1);
+			}
 		}
 	    return os;
 	};
 
 	std::ostream& operator<<(std::ostream& os, const core* element) {
-		uchar bit_size = element->label_length;
-		while (bit_size--) {
-			os << ( (element->label >> bit_size) & 1 );
+		int bit_size = element->block_number * SIZE_PER_BLOCK - element->start_index;
+		for (int index=0; index<bit_size; index++) {
+			os << element->get(index);
 		}
 	    return os;
 	};
+
 };
