d219e17c7706c5aa9156734968f9e3a44494c13d
Begüm Filiz Öz <113853370+begumfilizoz@users.noreply.github.com>
2024-10-19 22:58:08 +0300

    Helper methods are updated

--- utils/helper.cpp
+++ utils/helper.cpp
@@ -19,12 +19,14 @@
 #include <chrono>
 #include <iomanip>
 #include <sstream>
-
+#include <string>
 
 #define DISTANCE_LENGTH     10000
 #define LCP_LEVEL           8
-#define KMER_SIZE           10
-#define WINDOW_SIZE         15
+
+
+// to represent 15 chars = uint32_t, 31 chars = uint64_t, 63 chars = unsigned __int128
+typedef uint32_t kmer_type;
 
 
 /**
@@ -124,6 +126,106 @@ double stdev(int (&numbers)[DISTANCE_LENGTH], std::vector<int> numbersXL = {}) {
         variance += ( ( mean_value - numbersXL[i] ) * ( mean_value - numbersXL[i] ) );
     }
     return sqrt(variance / count);
+};
+
+
+/**
+ * @brief Calculates the mean of distances.
+ *
+ * This function computes the weighted mean of an array of distances. Each index
+ * in the array corresponds to a distance, and the value at that index represents
+ * how many times that distance occurs.
+ *
+ * @param distances Pointer to an array of integers where each element represents
+ *                  the count of occurrences of the corresponding index distance.
+ * @param n         The size of the distances array.
+ * @return The weighted mean of the distances.
+ */
+double mean(int* distances, int n) {
+    double numOfDistances = 0;
+    double totalDistance = 0;
+    for (int i = 0; i < n; i++)
+    {
+        totalDistance += i * distances[i];
+        numOfDistances += distances[i];
+    }
+    return totalDistance / numOfDistances;
+};
+
+
+/**
+ * @brief Calculates the standard deviation of distances.
+ *
+ * This function computes the standard deviation of distances based on their
+ * frequency in the array. The mean value is provided as a parameter for use in the
+ * calculation.
+ *
+ * @param distances Pointer to an array of integers representing the counts of each
+ *                  distance.
+ * @param n         The size of the distances array.
+ * @param mean      The precomputed mean of the distances.
+ * @return The standard deviation of the distances.
+ */
+double stdev(int* distances, int n, double mean) {
+    double sum = 0;
+    double N = 0;
+    for (int i = 0; i < n; i++)
+    {
+        N += distances[i];
+        sum += pow(((i) - mean), 2) * distances[i];
+    }
+    return sqrt(sum / N);
+};
+
+
+/**
+ * @brief Encodes a substring of DNA characters into a unique unsigned integer.
+ *
+ * This function encodes a sequence of characters (typically representing nucleotides
+ * in a DNA string) using a predefined mapping into a 2-bit encoded integer for each
+ * character. The final encoded value is returned as an unsigned integer.
+ *
+ * @param map    Pointer to an array where each index represents the mapping of a character.
+ * @param begin  Iterator to the beginning of the substring to encode.
+ * @param end    Iterator to the end of the substring to encode.
+ * @return A unique unsigned integer representing the encoded DNA sequence.
+ */
+kmer_type encode(int* map, std::string::iterator begin, std::string::iterator end) {
+    kmer_type res = 0;
+    for ( std::string::iterator it = begin; it < end; it++ )
+    {
+        res *= 4;
+        res |= map[static_cast<size_t>(*it)];
+    }
+    return res;
+};
+
+
+/**
+ * @brief Initializes a character map for nucleotide encoding.
+ *
+ * This function initializes a mapping of DNA characters (A, T, G, C) and their lowercase
+ * counterparts (a, t, g, c) into integers (0, 3, 2, 1, respectively). All other characters
+ * are mapped to 0 by default.
+ *
+ * @param map A 128-element integer array where each index corresponds to a character
+ *            in the ASCII table.
+ */
+void init_map(int map[128]) {
+    for ( int i = 0; i < 128; i++ )
+    {
+        map[i] = 0;
+    }
+
+    map['A'] = 0;
+    map['T'] = 3;
+    map['G'] = 2;
+    map['C'] = 1;
+    // Assume there are soft masks
+    map['a'] = 0;
+    map['t'] = 3;
+    map['g'] = 2;
+    map['c'] = 1;
 };
 
 
